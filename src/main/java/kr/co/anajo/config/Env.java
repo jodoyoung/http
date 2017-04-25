b9b0c5bbbbd5259c44aa5925a9d31db7241c8352
jodoyoung <jodoyoung36@gmail.com>
2017-04-25 23:41:19 +0900

    static resource path

--- src/main/java/kr/co/anajo/config/Env.java
+++ src/main/java/kr/co/anajo/config/Env.java
@@ -2,10 +2,8 @@ package kr.co.anajo.config;
 
 import java.nio.file.Paths;
 
-import io.netty.util.internal.SystemPropertyUtil;
-
 public interface Env {
 
-	String docRoot = Paths.get(SystemPropertyUtil.get("user.dir")).resolve("src").resolve("main").resolve("static").toString();
+	String docRoot = Paths.get(Env.class.getResourceAsStream("/").toString()).resolve("static").toString();
+
 }
-	
